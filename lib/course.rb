b2e8810528e07b70dd5f3d269c972a8f14c75348
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-16 17:23:04 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,11 +3,17 @@ class Course
   # attr_accessor :description
   # attr_accessor :schedule 
   attr_accessor :title, :schedule, :description
+  @@all = []
   
   def initialize(title, schedule, description)
     @title = title
     @schedule = schedule
     @description = description
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
   end
 end
 
